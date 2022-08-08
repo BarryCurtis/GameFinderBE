8fb6263f40cf90a636e0050de580d96b7956d232
BarryCurtis <barrycurtis@hotmail.com>
2022-08-08 16:40:11 +0100

    set up database files

---
+++ data/comments.js
@@ -0,0 +1,7 @@
+// Comments;
+// {
+//   comment_id: PRIMARY_KEY;
+//   event_id: FOREIGN_KEY;
+//   user_id: FOREIGN_KEY;
+//   comment_body: TEXT;
+// }
---
+++ data/users.js
@@ -0,0 +1,13 @@
+// Users
+// {
+// user_id: PRIMARY_KEY
+// name: TEXT
+// username: TEXT
+// email: TEXT
+// age: INTEGER
+// password: TEXT
+// profile_icon: TEXT (img_url selection)
+// skills_level: TEXT
+// rating: INT
+// events_booked: FOREIGN_KEY
+// }
