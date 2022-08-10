20c0b556548e63e7af4a779b8b70502c07350036
BarryCurtis <barrycurtis@hotmail.com>
2022-08-10 12:44:07 +0100

    Heroku

--- public/db/connection.js
+++ public/db/connection.js
@@ -4,13 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 const pg_1 = require("pg");
 const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config({
-    path: `${__dirname}/../../.env`,
-});
-const config = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-};
+let config = {};
+if (process.env.NODE_ENV === "production") {
+    config = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    };
+}
+else {
+    dotenv_1.default.config({
+        path: `${__dirname}/../../.env`,
+    });
+    config = {};
+}
 module.exports = new pg_1.Pool(config);
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,16 +1,18 @@
 import {Pool} from "pg";
-
-
 import dotenv from 'dotenv';
-
-dotenv.config({
-    path: `${__dirname}/../../.env`,
-  });
-
-  const config = {
+let config = {};
+if (process.env.NODE_ENV === "production")
+{config = {
         connectionString: process.env.DATABASE_URL,
         ssl: {
           rejectUnauthorized: false,
-        },
-      }
+        },}}
+else
+{dotenv.config({
+    path: `${__dirname}/../../.env`,
+  });
+config = {}}
+
+
+  
 export = new Pool(config)
