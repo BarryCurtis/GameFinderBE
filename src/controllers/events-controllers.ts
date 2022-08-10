60d910c0d4599567cbd04ec57a0efbea55fd164d
BarryCurtis <barrycurtis@hotmail.com>
2022-08-10 08:58:32 +0100

    add initial query

--- src/controllers/events-controllers.ts
+++ src/controllers/events-controllers.ts
@@ -1,4 +1,4 @@
-import {fetchEvents} from "../models/events-models"
+import {fetchEvents, fetchEventsByFilter} from "../models/events-models"
 
 export const getEvents = (req, res, next) => {
     return fetchEvents()
@@ -9,3 +9,75 @@ export const getEvents = (req, res, next) => {
         next(err);
     });
 }
+
+export const getEventsByFilter = (req, res, next) => {
+    return fetchEventsByFilter()
+    .then((events) => {
+        res.status(200).send({events: events});
+    })
+    .catch((err)=>{
+        next(err);
+    })
+}
+
+
+
+exports.fetchEvents = async (
+  sort_by = "category",
+  order = "desc",
+  filter = null
+) => {
+  const validSort_by = [
+    "category",
+    "gender",
+    "",
+    "",
+  ];
+  const validOrder = ["ASC", "DESC", "asc", "desc"];
+  if (!validSort_by.includes(sort_by)) {
+    return Promise.reject({
+      status: 400,
+      msg: `cannot sort by ${sort_by}`,
+    });
+  }
+  if (!validOrder.includes(order)) {
+    return Promise.reject({
+      status: 400,
+      msg: `cannot sort by ${order}`,
+    });
+  }
+  const topicValidate = await connection.query(
+    `SELECT * FROM topics WHERE slug = $1`,
+    [topic]
+  );
+  if (topic) {
+    if (topicValidate.rowCount === 0) {
+      return Promise.reject({
+        status: 400,
+        msg: `cannot sort by ${topic}`,
+      });
+    } else {
+      return connection.query(
+        `SELECT articles.*, COUNT(comments.article_id)::INT
+        AS comment_count
+        FROM articles
+        LEFT JOIN comments ON comments.article_id = articles.article_id
+        WHERE topic = $1
+        GROUP BY articles.article_id
+        ORDER BY ${sort_by} ${order}`,
+        [topic]
+      ).then((result)=> {
+        return result.rows
+      })
+    }
+  } else {
+    return connection.query(
+      `SELECT articles.*, COUNT(comments.article_id)::INT
+      AS comment_count
+      FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id
+      GROUP BY articles.article_id
+      ORDER BY ${sort_by} ${order}`
+    ).then((result)=> {
+      return result.rows
+  })
+  }}
--- src/models/events-models.ts
+++ src/models/events-models.ts
@@ -1,3 +1,4 @@
+import { ReadyForQueryMessage } from "pg-protocol/dist/messages";
 import db from "../db/connection";
 
 export const fetchEvents = () => {
@@ -7,3 +8,43 @@ export const fetchEvents = () => {
   });
 };
 
+export const fetchEventsByFilter = (sort_by = "time", order = "DESC", filter) => {
+
+    const event_columns = [
+    "event_id",
+    "firebase_id",
+    "category",
+    "date",
+    "time",
+    "duration",
+    "gender",
+    "skills_level",
+    "location",
+    "needed_players",
+    "age_group",
+    "cost",
+    ];
+
+    const filter_columns = 
+    ["gender", "category", "age_group"];
+
+    if (!event_columns.includes(sort_by)) {
+        return Promise.reject({status:400, msg: "Sort by query invalid"})
+    }
+    if (!["ASC", "DESC", "asc", "desc"].includes(order)) {
+    return Promise.reject({ status: 400, msg: "Order by query invalid" });
+  }
+
+  let queryStr = "";
+
+  if (!filter){queryStr.concat(sort_by, order)}
+  else {queryStr.concat(sort_by, order, filter)}
+
+
+  
+
+
+    return db.query(`SELECT * FROM event`).then((result) =>{
+        return result.rows;
+    })
+}
