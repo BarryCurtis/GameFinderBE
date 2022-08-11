06ec36926bcfc43961076896b2c94c56187ce9bf
beyarDev <beyarDev@gmail.com>
2022-08-11 13:05:45 +0100

    add test for invalid post requests

--- public/models/comments-models.js
+++ public/models/comments-models.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.addCommentBYEventsId = exports.fetchCommentsBYEventsId = void 0;
 const connection_1 = __importDefault(require("../db/connection"));
+const checkExist_1 = require("../utiles/checkExist");
 const fetchCommentsBYEventsId = (event_id) => {
     if (isNaN(Number(event_id))) {
         return Promise.reject({
@@ -26,13 +27,21 @@ const fetchCommentsBYEventsId = (event_id) => {
 };
 exports.fetchCommentsBYEventsId = fetchCommentsBYEventsId;
 const addCommentBYEventsId = (event_id, firebase_id, comment_body, comment_time) => {
-    console.log(event_id);
-    return connection_1.default
-        .query(`INSERT INTO comments
+    console.log(event_id, firebase_id, comment_body, comment_time);
+    if (comment_body === "") {
+        return Promise.reject({
+            status: 400,
+            msg: `cannot post empty comment`,
+        });
+    }
+    return (0, checkExist_1.checkExist)("events", "event_id", event_id).then(() => {
+        return connection_1.default
+            .query(`INSERT INTO comments
         (event_id, firebase_id, comment_body, comment_time) VALUES ($1, $2, $3, $4) 
         RETURNING *`, [event_id, firebase_id, comment_body, comment_time])
-        .then((result) => {
-        return result.rows[0];
+            .then((result) => {
+            return result.rows[0];
+        });
     });
 };
 exports.addCommentBYEventsId = addCommentBYEventsId;
